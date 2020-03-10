01df519c86fb3627713f172cc316d22fb0aa04ec
Компик Наташи) < )@DESKTOP-KEF0OEP>
2020-03-10 21:25:57 +0300

    created programm file

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            short a1 = 1;
+            object obj1 = a1;
+            System.SByte a2 = (System.SByte)a1; 
+
+            Console.WriteLine(a2);
+        }
+    }
+}
