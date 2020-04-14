6efa99ceabe98eafb540383d92023eb93e71ad3c
SergeiPodobed <izotep19@yandex.ru>
2020-04-14 22:21:48 +0300

    Shapes start

---
+++ Lectures_11.Shapes/Lectures_11.Shapes_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lectures_11.Shapes_
+{
+    class Program
+    {
+        object quadrat;
+        object rectangle;
+        object circle;
+        Random rnd = new Random();
+        int a = rnd.Next(10);
+        
+        static void area (int[] args)
+        {
+            area quadrat = new area();
+           
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
