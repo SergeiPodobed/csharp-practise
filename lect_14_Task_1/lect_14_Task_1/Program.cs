e6a1ed8812f7e8a86e066ca355a6293a596b44b4
SergeiPodobed <izotep19@yandex.ru>
2020-04-23 20:56:04 +0300

    task 1 lect 14 needs rebuilding, must learn materilas

---
+++ lect_14_Task_1/lect_14_Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lect_14_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, guys";
+            string text1 = text.StringAdd();
+            Console.WriteLine(text1);
+
+            Console.Read();
+        }
+    }
+
+    public static class StringExt
+    {
+        public static string StringAdd(this string str)
+        {
+
+            str = str.Substring(5);
+            str = str.Insert(6, "...");
+
+        }
+    }
+}
